1dd81a2d65c2f4cdb94ece91e4cd68175926f082
Jhobany Peña <jhpenaor@microsoft.com>
2026-04-26 14:54:33 -0500

    Refactor CaseStudyOne and BrandOne components for improved styling consistency

--- src/app/page.js
+++ src/app/page.js
@@ -24,10 +24,8 @@ export default function Home() {
         <SolutionOne data={serviceData} start={0} limit={6} />
         {/* ServiceOne oculto temporalmente */}
         {/* <ServiceOne data={serviceData} start={6} limit={16} /> */}
-        <CaseStudyOne classname={'bg-linear lg:mt-20 sm:mt-14 mt-10 lg:py-20 sm:py-14 py-10'} data={caseStudyData} start={0} limit={5} />
-        {/* BannerOne oculto temporalmente */}
-        {/* <BannerOne /> */}
-        <BrandOne classname={'bg-white lg:py-20 md:py-14 py-10'} />
+        <CaseStudyOne classname={'bg-white lg:mt-0 lg:py-20 sm:py-14 py-10'} data={caseStudyData} start={0} limit={5} />
+        <BrandOne classname={'bg-linear lg:py-20 md:py-14 py-10'} />
         {/* TestimonialOne oculto temporalmente */}
         {/* <TestimonialOne data={testimonialData} /> */}
         <ContactOne classname={'bg-linear-blue'} />
